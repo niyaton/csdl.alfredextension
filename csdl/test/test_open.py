014b17eca5f31f0925a5ce34d66ec301e79285e1
Kenji Fujiwara <kenji-f@is.naist.jp>
2012-12-22 03:43:49 +0900

    modify csdl command

--- csdl/test/test_open.py
+++ csdl/test/test_open.py
@@ -6,7 +6,7 @@ def test_get_conference_page_url():
     url = get_conference_page_url('ase')
     assert url == 'http://www.computer.org/csdl/proceedings/ase/index.html'
 
-def test_get_conference_page_by_url():
+def test_get_conference_page_url_by_year():
     url = get_conference_page_url_by_year('ase', 2000)
     assert url == 'http://www.computer.org/csdl/proceedings/ase/2000/0710/00/index.html'
 
